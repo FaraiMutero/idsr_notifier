dd23344799a9e23b33e31c6a288e2b35213b843d
Farai Mutero <fmutero@gmail.com>
2023-09-19 09:24:22 +0200

    Added notifications at same orgunit

--- routers/api.js
+++ routers/api.js
@@ -4,7 +4,6 @@ var router = express.Router();
 router.get('/', function (req, res) {
     console.log('API landing reached. /api endpoint @ ' + new Date().toISOString())
     res.send('You have arrived at the /api endpoint. Timestamp : ' + new Date().toISOString());
-    utils.login()
 })
 
 module.exports = router;
